62d58c66e39324ce603857537bcac65fe4e68a4f
Yesl Kwon <yesl0426@naver.com>
2022-12-19 12:03:34 +0900

    현실 씬_마지막전 푸시
    UI를 추가함
    U키 추가 기능 업데이트 필요

--- Assets/_GroundScene/UI/Button.cs
+++ Assets/_GroundScene/UI/Button.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyButton : MonoBehaviour
+public class Button : MonoBehaviour
 {
     public GameObject Button1;
     public GameObject Panel1;
