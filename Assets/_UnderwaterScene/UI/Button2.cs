6ab8a292e35346bf6bccdb590fbb0e879076ef40
w0n11 <113477114+w0n11@users.noreply.github.com>
2022-12-19 13:43:41 +0900

    button added
    .

--- Assets/_UnderwaterScene/UI/Button2.cs
+++ Assets/_UnderwaterScene/UI/Button2.cs
@@ -7,6 +7,8 @@ public class Button2 : MonoBehaviour
     public GameObject Canvas;
     public GameObject ScrollView;
     public GameObject Text;
+    private bool isMenuActive = false;
+
 
     void Start()
     {
@@ -21,10 +23,30 @@ public class Button2 : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.U)){
+        /*if (Input.GetKeyDown(KeyCode.U) && Text.Inactive){
             Text.SetActive(true);
             ScrollView.SetActive(true);
+        }*/
 
+        if (Input.GetKeyDown(KeyCode.U)){
+        if (isMenuActive)
+        {
+            Text.SetActive(false);
+            ScrollView.SetActive(false);
+            isMenuActive = false;   
         }
+        else
+        {
+            Text.SetActive(true);
+            ScrollView.SetActive(true);
+            isMenuActive = true;
+        }
+ }
+
+        /*if (Input.GetKeyDown(KeyCode.H) && lightsMode == LightsMode.Inactive){
+            lightsMode = LightsMode.Active;
+            }
+     else if (Input.GetKeyDown(KeyCode.H) && lightsMode == LightsMode.Active){
+            lightsMode = LightsMode.Inactive;}*/
     }
 }
